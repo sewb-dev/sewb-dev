78c8aee9cca6a698e038b3beb421c7965163c894
Temiloluwa Philip Ojo <codekagei@gmail.com>
2023-09-08 16:35:16 +0100

    fix: remove export for build

--- src/app/api/users/waiting-list/route.ts
+++ src/app/api/users/waiting-list/route.ts
@@ -4,26 +4,36 @@ import { getDatabase, ref, set } from "firebase/database";
 import crypto from "crypto";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  console.log("111111");
-  const { email, fullName } = await req.json();
-  const combinedString = email;
-  const hash = crypto.createHash("sha256").update(combinedString).digest("hex");
+  try {
+    const { email, fullName } = await req.json();
+    const combinedString = email;
+    const hash = crypto
+      .createHash("sha256")
+      .update(combinedString)
+      .digest("hex");
 
-  const database = getDatabase(firebase);
+    const database = getDatabase(firebase);
 
-  await set(ref(database, "users/" + hash), {
-    username: fullName,
-    email: email,
-  });
+    await set(ref(database, "users/" + hash), {
+      username: fullName,
+      email: email,
+    });
 
-  return new Response(
-    JSON.stringify({
-      message: `Success`,
-    }),
-    {
-      status: 200,
-    }
-  );
+    console.log(
+      `Successfully added user with hash=${hash} to waiting-list database.`
+    );
+
+    return new Response(
+      JSON.stringify({
+        message: `Success`,
+      }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function GET(req: NextRequest, res: NextResponse) {
