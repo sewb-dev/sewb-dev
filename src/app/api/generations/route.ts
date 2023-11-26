487804e6432426aad84fceeb0d8b5c959c4ba991
Temiloluwa Philip Ojo <codekagei@gmail.com>
2023-11-26 12:34:09 +0100

    complete streaming feature

--- src/app/api/generations/route.ts
+++ src/app/api/generations/route.ts
@@ -1,6 +1,6 @@
 import { GenerationModelDto, GenerationQNAIDto } from '@/dto/generation';
 import { config } from '@/lib/auth';
-import generationService from "@/modules/generation/generation.service";
+import generationService from '@/modules/generation/generation.service';
 import { StatusCodes } from 'http-status-codes';
 import { getServerSession } from 'next-auth/next';
 import { NextRequest, NextResponse } from 'next/server';
@@ -8,10 +8,13 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    const session = await getServerSession(config)
+    const session = await getServerSession(config);
 
     if (!session) {
-      return NextResponse.json({ message: 'You must be logged in' }, { status: StatusCodes.UNAUTHORIZED })
+      return NextResponse.json(
+        { message: 'You must be logged in' },
+        { status: StatusCodes.UNAUTHORIZED }
+      );
     }
 
     const { numberOfQuestions, sourceText } = await req.json();
@@ -23,26 +26,36 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const { email } = session.user
-    const { qnai, generation } = await generationService.createQNAGeneration(email, sourceText, numberOfQuestions)
+    const { email } = session.user;
+    const { qnai, generation } = await generationService.createQNAGeneration(
+      email,
+      sourceText,
+      numberOfQuestions
+    );
 
-    if (!(qnai && generation) ) {
-      return NextResponse.json({ message: 'An error occured while saving the generation' }, { status: StatusCodes.INTERNAL_SERVER_ERROR })
+    if (!(qnai && generation)) {
+      return NextResponse.json(
+        { message: 'An error occured while saving the generation' },
+        { status: StatusCodes.INTERNAL_SERVER_ERROR }
+      );
     }
 
     const generationResponse: GenerationModelDto = {
       ...generation,
-      generatedAt: (new Date(generation.generatedAt)).toISOString()
-    }
+      generatedAt: new Date(generation.generatedAt).toISOString(),
+    };
 
     const response: GenerationQNAIDto = {
       qnai,
-      generation: generationResponse
-    }
+      generation: generationResponse,
+    };
 
-    return NextResponse.json(response, { status: StatusCodes.CREATED })
+    return NextResponse.json(response, { status: StatusCodes.CREATED });
   } catch (error) {
     console.error('Error processing the request:', error);
-    return NextResponse.json({ error: 'Server Error' }, { status: StatusCodes.INTERNAL_SERVER_ERROR });
+    return NextResponse.json(
+      { error: 'Server Error' },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR }
+    );
   }
 }
